c93eebf28cb47f9cfd27e742a82e5b6e071682fb
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-11-17 12:48:15 +0100

    Building blocks

---
+++ 06-building-blocks/index.js
@@ -0,0 +1,43 @@
+const btnStart = document.getElementById('btnStart');
+const btnStop = document.getElementById('btnStop');
+const result = document.getElementById('result');
+
+btnStart.addEventListener('click', () => {});
+
+btnStop.addEventListener('click', () => {});
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+if (module.hot) {
+  module.hot.dispose(function() {
+    location.reload();
+  });
+}
