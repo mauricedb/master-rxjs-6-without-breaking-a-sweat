36caed64e69f8ae0473a83b376aa2ea3363e30a0
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-11-21 19:14:56 +0100

    Hot and cold

---
+++ 16-hot-cold/index.js
@@ -0,0 +1,84 @@
+import { Subject, Observable } from 'rxjs';
+
+const btnStartCold = document.getElementById('btnStartCold');
+const btnStartHot = document.getElementById('btnStartHot');
+const result = document.getElementById('result');
+
+function coldObservable() {
+  return Observable.create(subscriber => {
+    let count = 0;
+    const handle = setInterval(() => {
+      const value = count++;
+      console.log('Cold', value);
+      subscriber.next(value);
+    }, 100);
+
+    return () => clearInterval(handle);
+  });
+}
+
+function hotObservable() {
+  const subject = new Subject();
+  let count = 0;
+  setInterval(() => {
+    const value = count++;
+    console.log('Hot', value);
+    subject.next(value);
+  }, 100);
+
+  return subject;
+}
+
+
+
+
+
+
+
+const cold = coldObservable();
+btnStartCold.addEventListener('click', () => {
+  cold.subscribe(time => (result.textContent = time));
+});
+
+
+
+const hot = hotObservable();
+btnStartHot.addEventListener('click', () => {
+  hot.subscribe(time => (result.textContent = time));
+});
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+if (module.hot) {
+  module.hot.dispose(function() {
+    location.reload();
+  });
+}
