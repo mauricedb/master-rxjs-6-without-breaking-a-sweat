6b71d97f061fb53baa849b9586bbfd81ff7f92a9
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-11-25 15:23:35 +0100

    Rename

---
+++ 19-map/index.js
@@ -0,0 +1,51 @@
+import { fromEvent } from 'rxjs';
+
+const canvas = document.getElementById('canvas');
+
+fromEvent(canvas, 'mousemove')
+  .subscribe(pos => {
+    pos.ctx.fillRect(pos.x, pos.y, 2, 2);
+  });
+
+const rect = canvas.getBoundingClientRect();
+const ctx = canvas.getContext('2d');
+const scale = window.devicePixelRatio;
+canvas.width = rect.width * scale;
+canvas.height = rect.height * scale;
+ctx.scale(scale, scale);
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+if (module.hot) {
+  module.hot.dispose(function() {
+    location.reload();
+  });
+}
