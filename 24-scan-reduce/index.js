22d69eb8a377e84a7c044394559287e381e76bd1
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-12-03 20:04:20 +0100

    Scan reduce pairwise

---
+++ 24-scan-reduce/index.js
@@ -0,0 +1,55 @@
+import { range, interval } from 'rxjs';
+import { tap, scan, reduce, take } from 'rxjs/operators';
+
+const btnStart = document.getElementById('btnStart');
+const result = document.getElementById('result');
+
+btnStart.addEventListener('click', e => {
+  // range(1, 5)
+  interval(500)
+    .pipe(
+      take(10),
+      tap(n => console.log(n)),
+      reduce((previous, current) => previous + current, 0)
+      // scan((previous, current) => previous + current, 0)
+    )
+    .subscribe({
+      next: item => (result.textContent = item)
+    });
+});
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+if (module.hot) {
+  module.hot.dispose(function() {
+    location.reload();
+  });
+}
---
+++ 25-pairwise/index.js
@@ -0,0 +1,67 @@
+import { fromEvent } from 'rxjs';
+import { map, pairwise } from 'rxjs/operators';
+
+const canvas = document.getElementById('canvas');
+
+fromEvent(canvas, 'mousemove')
+  .pipe(
+    map(e => ({
+      ctx: e.target.getContext('2d'),
+      x: e.offsetX,
+      y: e.offsetY
+    })),
+    pairwise()
+  )
+  .subscribe(pair => {
+    // pos.ctx.fillRect(pos.x, pos.y, 2, 2);
+    const [from, to] = pair;
+    const ctx = from.ctx;
+
+    ctx.beginPath();
+    ctx.moveTo(from.x, from.y);
+    ctx.lineTo(to.x, to.y);
+    ctx.stroke();
+  });
+
+const rect = canvas.getBoundingClientRect();
+const ctx = canvas.getContext('2d');
+const scale = window.devicePixelRatio;
+canvas.width = rect.width * scale;
+canvas.height = rect.height * scale;
+ctx.scale(scale, scale);
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+if (module.hot) {
+  module.hot.dispose(function() {
+    location.reload();
+  });
+}
