172707d184da6f7abe56ecf3548d3af247f7883b
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-12-04 10:54:26 +0100

    startWith

---
+++ 27-start-with/index.js
@@ -0,0 +1,56 @@
+import { fromEvent, interval } from 'rxjs';
+import { take, scan, map, switchMap, startWith } from 'rxjs/operators';
+
+const btnStart = document.getElementById('btnStart');
+const result = document.getElementById('result');
+
+fromEvent(btnStart, 'click')
+  .pipe(
+    scan(previous => previous + 1, 0),
+    switchMap(click =>
+      interval(2500).pipe(
+        map(i => `Click ${click} event ${i}`),
+        startWith('Start'),
+        take(10)
+      )
+    )
+  )
+  .subscribe({
+    next: item => (result.textContent = item)
+  });
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+if (module.hot) {
+  module.hot.dispose(function() {
+    location.reload();
+  });
+}
