e9255dc7c1a96763106d256832c865eab7513a7a
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-12-04 10:40:52 +0100

    Mapping observables

---
+++ 26-star-map/index.js
@@ -0,0 +1,69 @@
+import { fromEvent, range, interval } from 'rxjs';
+import {
+  take,
+  scan,
+  map,
+  mergeMap,
+  concatMap,
+  switchMap
+} from 'rxjs/operators';
+
+const btnStart = document.getElementById('btnStart');
+
+function createSecondObservable(click) {
+  // return range(1, 5)
+  //   .pipe(map(i => `Click ${click} event ${i}`));
+
+  return interval(1000).pipe(
+    map(i => `Click ${click} event ${i}`),
+    // take(5)
+  );
+}
+
+fromEvent(btnStart, 'click')
+  .pipe(
+    scan(previous => previous + 1, 0),
+    // mergeMap(click => createSecondObservable(click)),
+    // concatMap(click => createSecondObservable(click)),
+    switchMap(click => createSecondObservable(click)),
+  )
+  .subscribe({
+    next: console.log,
+    complete: () => console.log('complete')
+  });
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+if (module.hot) {
+  module.hot.dispose(function() {
+    location.reload();
+  });
+}
