bf9fc7759af84a0599a134f0f9be9a47e4c3c944
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-12-11 10:32:13 +0100

    Retrying Errors

---
+++ 402-retry-errors/index.js
@@ -0,0 +1,58 @@
+import { fromEvent, interval } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { switchMap, take, retry, retryWhen } from 'rxjs/operators';
+
+const btnStart = document.getElementById('btnStart');
+const result = document.getElementById('result');
+
+fromEvent(btnStart, 'click')
+  .pipe(
+    switchMap(() =>
+      ajax.getJSON('http://theproblemsolver.nl/api/not-there').pipe(
+        // retry(3)
+
+        retryWhen(err => interval(1000).pipe(take(3)))
+      )
+    )
+  )
+  .subscribe({
+    next: item => (result.textContent = item),
+    error: error => (result.textContent = `Error: ${error.message}`),
+    complete: () => (result.textContent = 'Complete')
+  });
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+if (module.hot) {
+  module.hot.dispose(function() {
+    location.reload();
+  });
+}
