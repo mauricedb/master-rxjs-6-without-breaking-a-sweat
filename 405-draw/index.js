e5f1277d55c6b9cb5ee1ce7878bbab36b4a328ff
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-12-15 16:39:06 +0100

    Drawing

---
+++ 405-draw/index.js
@@ -0,0 +1,77 @@
+import { fromEvent } from 'rxjs';
+import { map, pairwise, switchMap, takeUntil } from 'rxjs/operators';
+
+const canvas = document.getElementById('canvas');
+
+const mouseDown$ = fromEvent(canvas, 'mousedown');
+const mouseMove$ = fromEvent(canvas, 'mousemove');
+const mouseUp$ = fromEvent(canvas, 'mouseup');
+const mouseOut$ = fromEvent(canvas, 'mouseout');
+
+mouseDown$
+  .pipe(
+    switchMap(() =>
+      mouseMove$.pipe(
+        map(e => ({
+          ctx: e.target.getContext('2d'),
+          x: e.offsetX,
+          y: e.offsetY
+        })),
+        pairwise(),
+        takeUntil(mouseUp$),
+        takeUntil(mouseOut$)
+      )
+    )
+  )
+  .subscribe(pair => {
+    const [from, to] = pair;
+    const ctx = from.ctx;
+
+    ctx.beginPath();
+    ctx.moveTo(from.x, from.y);
+    ctx.lineTo(to.x, to.y);
+    ctx.stroke();
+  });
+
+const rect = canvas.getBoundingClientRect();
+const ctx = canvas.getContext('2d');
+const scale = window.devicePixelRatio;
+canvas.width = rect.width * scale;
+canvas.height = rect.height * scale;
+ctx.scale(scale, scale);
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+if (module.hot) {
+  module.hot.dispose(function() {
+    location.reload();
+  });
+}
