2ca5a9be433fd2dfaeb6c100f6462b300d4da1f4
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-07-22 16:39:40 +0200

    Note about ajax.getJSON()

--- 404-ajax-polling/index.js
+++ 404-ajax-polling/index.js
@@ -1,5 +1,5 @@
-import { EMPTY, timer, map, catchError, mergeMap, switchMap, tap } from 'rxjs';
-import { ajax, mapResponse } from 'rxjs/ajax';
+import { EMPTY, timer, map, catchError, mergeMap, switchMap, tap  } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
 const url =
   'http://api.icndb.com/jokes/random/10/?limitTo=[nerdy]&escape=javascript';
@@ -9,7 +9,12 @@ const result = document.getElementById('result');
 const joke$ = timer(0, 5000).pipe(
   tap(() => (result.innerHTML = '')),
   switchMap(() =>
+    // Old: RxJS 6
+    // Fails because of an extra x-requested-with header
+    // is not allowed with the CORS request
     // ajax.getJSON(url).pipe(catchError(() => EMPTY))
+
+    // New code. Works with RxJS 6 and 7.
     ajax({url, crossDomain: true}).pipe(map(e => e.response), catchError(() => EMPTY))
   ),
   mergeMap((rsp) => rsp.value)
